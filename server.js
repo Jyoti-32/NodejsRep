8f17ec1034978117a49035351b76369c912a6063
Jyoti-32 <jyoti.pt49@gmail.com>
2019-04-03 13:46:54 +0530

    start script and HEroku Port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express=require('express');
 const hbs=require('hbs');
 const fs=require('fs');
+const port=process.env.PORT||3000;  //heroku
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -36,4 +37,6 @@ app.get('/about',(req,res)=>{
 });
 
 
-app.listen(3000);
+app.listen(port, ()=>{
+	console.log('server is up on port ${port}');
+});
